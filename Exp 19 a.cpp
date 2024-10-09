ae51e6cb068695862f7179cea80670d9a6a5a723
snehadiwan04 <diwansneha311204@gmail.com>
2024-10-09 09:49:30 +0530

    Add files via upload

---
+++ Exp 19 a.cpp
@@ -0,0 +1,88 @@
+//sneha diwaan
+//entc B2
+//23070123126
+//experiment 19
+#include <iostream>
+using namespace std;
+int queue[100], n = 100, front = - 1, rear = - 1;
+void Insert() {
+   int val;
+   if (rear == n - 1)
+   cout<<"Queue Overflow"<<endl;
+   else {
+      if (front == - 1)
+      front = 0;
+      cout<<"Insert the element in queue : "<<endl;
+      cin>>val;
+      rear++;
+      queue[rear] = val;
+   }
+}
+void Delete() {
+   if (front == - 1 || front > rear) {
+      cout<<"Queue Underflow ";
+      return ;
+   } else {
+      cout<<"Element deleted from queue is : "<< queue[front] <<endl;
+      front++;;
+   }
+}
+void Display() {
+   if (front == - 1)
+   cout<<"Queue is empty"<<endl;
+   else {
+      cout<<"Queue elements are : ";
+      for (int i = front; i <= rear; i++)
+      cout<<queue[i]<<" ";
+         cout<<endl;
+   }
+}
+int main() {
+   int ch;
+   cout<<"1) Insert element to queue"<<endl;
+   cout<<"2) Delete element from queue"<<endl;
+   cout<<"3) Display all the elements of queue"<<endl;
+   cout<<"4) Exit"<<endl;
+   do {
+      cout<<"Enter your choice : "<<endl;
+      cin>>ch;
+      switch (ch) {
+         case 1: Insert();
+         break;
+         case 2: Delete();
+         break;
+         case 3: Display();
+         break;
+         case 4: cout<<"Exit"<<endl;
+         break;
+         default: cout<<"Invalid choice"<<endl;
+      }
+   } while(ch!=4);
+   return 0;
+}
+/*
+1) Insert element to queue
+2) Delete element from queue
+3) Display all the elements of queue
+4) Exit
+Enter your choice : 
+1
+Insert the element in queue : 
+44
+Enter your choice : 
+1
+Insert the element in queue : 
+55
+Enter your choice : 
+2
+Element deleted from queue is : 44
+Enter your choice : 
+3
+Queue elements are : 55 
+Enter your choice : 
+4
+Exit
+=== Code Execution Successful ===
+
+
+*/
